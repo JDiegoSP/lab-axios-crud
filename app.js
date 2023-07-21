d900528169ff460eb02c84ecc329bb518ffcfbf6
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-07-22 00:35:23 +0200

    Axios Crud

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const createError = require('http-errors');
-const express = require('express');
-const logger = require('morgan');
-const path = require('path')
+const createError = require("http-errors");
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
 
-require('./config/hbs.config');
+require("./config/hbs.config");
 
 const app = express();
 
@@ -11,32 +11,32 @@ const app = express();
  * Middlewares
  */
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"));
 
 /**
  * View setup
  */
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 /**
  * Configure routes
  */
-const router = require('./config/routes.config');
-app.use('/', router);
+const router = require("./config/routes.config");
+app.use("/", router);
 
 app.use((req, res, next) => {
-  next(createError(404, 'Page not found'));
+  next(createError(404, "Page not found"));
 });
 
 app.use((error, req, res, next) => {
   console.error(error);
   let status = error.status || 500;
 
-  res.status(status).render('error', {
+  res.status(status).render("error", {
     message: error.message,
-    error: req.app.get('env') === 'development' ? error : {},
+    error: req.app.get("env") === "development" ? error : {},
   });
 });
 
